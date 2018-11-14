baf7b90ce9e6df76b3ab2b4f4374b8f97ee50a7d
Hans <Contact.kamik423@gmail.com>
2018-11-14 16:55:33 +0100

    updated readme on pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('readme.md', encoding='utf-8') as file:
 
 setuptools.setup(
     name='cutie',
-    version='0.0.1',
+    version='0.0.2',
     author='Hans',
     author_email='contact.kamik423@gmail.com',
     description='Commandline User Tools for Input Easification',
