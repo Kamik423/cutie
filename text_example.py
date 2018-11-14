c729866500e4fa6b192cdbac047139d41af75402
Dylan Herrada <dylan.herrada@gmail.com>
2018-11-14 17:07:20 -0500

    Added example for the text argument

---
+++ text_example.py
@@ -0,0 +1,14 @@
+import __init__
+
+names = [
+        'Arthur, King of the Britons',
+        'Sir Lancelot the Brave',
+        'Sir Robin the Not-Quite-So-Brave-as-Sir-Lancelot',
+        'Sir Bedevere the Wise',
+        'Sir Galahad the Pure',
+	'Møøse']
+
+text = [
+        1]
+name = names[__init__.select(names, text, selected_index=2)]
+
