66042bf96764ced2e2b1953003eb203501fd8850
Hans <Contact.kamik423@gmail.com>
2018-11-15 10:10:54 +0100

    made downloadable on pypi again

--- setup.py
+++ setup.py
@@ -8,18 +8,21 @@ with open('readme.md', encoding='utf-8') as file:
 
 setuptools.setup(
     name='cutie',
-    version='0.1.0',
+    version='0.1.1',
     author='Hans',
     author_email='contact.kamik423@gmail.com',
     description='Commandline User Tools for Input Easification',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/kamik423/cutie',
-    packages=setuptools.find_packages(),
+    py_modules=['cutie'],
     license='MIT',
     install_requires=['colorama', 'readchar'],
     python_requires='>=3.5',
     classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
