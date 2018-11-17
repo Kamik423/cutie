e70159ce107d0d0586ea5dc11ed63bf2562560ce
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2018-11-17 16:29:56 +0100

    add documentation to yield_input and InputContext

--- test/__init__.py
+++ test/__init__.py
@@ -3,7 +3,41 @@ import readchar
 import cutie
 
 
+def yield_input(*data, raise_on_empty=False):
+    """
+    Closure that returns predefined data.
+
+    If the data is exhausted raise a MockException or reraise the IndexError
+    """
+    data = list(data)
+
+    def func(*a, **kw):
+        try:
+            return data.pop(0)
+        except IndexError as e:
+            if raise_on_empty:
+                raise MockException()
+            else:
+                raise e
+
+    return func
+
+
 class InputContext:
+    """
+    Context manager to simulate keyboard input returned by `readchar.readkey`,
+    by replacing it in `cutie` with `yield_input`
+
+    When the supplied keystrokes are exhausted a `MockException` will be raised.
+    This can be used to terminate the execution at any desired point, rather than
+    relying on internal control mechanisms.
+
+    Usage:
+        with InputContext(" ", "\r"):
+            cutie.select(["foo", "bar"])
+    This will issue a space and enter keypress, selecting the first item and
+    confirming.
+    """
 
     def __init__(self, *data, raise_on_empty=True):
         cutie.readchar.readkey = yield_input(*data, raise_on_empty=raise_on_empty)
@@ -17,15 +51,3 @@ class InputContext:
 
 class MockException(Exception):
     pass
-
-
-def yield_input(*data, raise_on_empty=False):
-    data = list(data)
-
-    def func(*a, **kw):
-        try:
-            return data.pop(0)
-        except IndexError:
-            raise MockException()
-
-    return func
