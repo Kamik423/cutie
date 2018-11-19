8b82c98380929e614af5fb89e2c519d0338a2d0a
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2018-11-19 18:59:39 +0100

    add print_call to tests

--- test/__init__.py
+++ test/__init__.py
@@ -3,6 +3,29 @@ import readchar
 import cutie
 
 
+def print_call(msg="", state="selectable", delimiter_selectable="()", delimiter_active="{}"):
+    """
+    Convenience function to generate expected calls to `cutie.print`
+    """
+    delimiter_selectable = tuple(delimiter_selectable)
+    delimiter_active = tuple(delimiter_active)
+    states = {
+                "selectable": 'K\x1b[1m%s %s\x1b[0m ' % delimiter_selectable,
+                "selected": 'K\x1b[1m(\x1b[32mx\x1b[0;1m)\x1b[0m ',
+                "caption": 'K',
+                "active": 'K\x1b[32;1m%s %s\x1b[0m ' % delimiter_active,
+                "active-selected": 'K\x1b[32;1m%sx%s\x1b[0m ' % delimiter_active,
+                "confirm": '1m%s%s confirm %s%s\x1b[0m \x1b[K' \
+                    % (
+                        delimiter_selectable[0], delimiter_selectable[0],
+                        delimiter_selectable[1], delimiter_selectable[1]
+                    ),
+                "confirm-active": '1;32m{{ confirm }}\x1b[0m \x1b[K',
+    }
+    base_call = '\x1b[{state}{message}'
+    return ((base_call.format(state=states[state], message=msg),),)
+
+
 def yield_input(*data, raise_on_empty=False):
     """
     Closure that returns predefined data.
